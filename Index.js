60c51593e1eb5f1d582c60dc8de15d635722e8a8
edfeld <edfeld1@gmail.com>
2018-10-19 23:00:11 -0700

    Word 50 complete

--- Index.js
+++ Index.js
@@ -4,3 +4,5 @@ var inquirer = require('inquirer')
 var Letter = require('./Letter.js')
 var Word = require('./Word.js')
 
+// let myTestWord = new Word("test");
+
--- Word.js
+++ Word.js
@@ -1,16 +1,27 @@
 // Word.js
 
 // Create the Word constructor
+
+var Letter = require("./Letter");
+
 var Word = function(strWord) {
     //
+    arrString = strWord.split("");
+    console.log("arrString: ", arrString);
     let arrWord = [];
+    for (let i = 0; i < arrString.length; i++) {
+        const element = new Letter(arrString[i]);
+        arrWord.push(element);
+    }
+    console.log("arrWord: ", arrWord);
 
-
-    this.displayPuzzle() {
-
+    this.displayPuzzle = function() {
+        console.log("displayPuzzle");
 
     }
   
   };
+
+//   let myTestWord = new Word("test");
   
   module.exports = Word;
